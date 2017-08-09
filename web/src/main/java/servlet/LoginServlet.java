9b96714b5379ec91540927514d5ac13ada1aea24
9yuan9 <xieshuyuanzh@163.com>
2017-08-09 15:42:01 +0800

    提交登录页面html和相关servlet

---
+++ web/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import dao.SqlSessionHelper;
+import dao.UserInfoDao;
+import entity.UserInfo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+@WebServlet(name = "LoginServlet",value="/toLogin")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserInfoDao dao= SqlSessionHelper.getSqlSession().getMapper(UserInfoDao.class);
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        String account = request.getParameter("username");
+        String pwd = request.getParameter("password");
+        UserInfo userInfo=new UserInfo();
+        userInfo.setUname(account);
+        userInfo.setPwd(pwd);
+        UserInfo user=dao.findUserNameAndPwd(userInfo);
+        if(user!=null){
+            out.print("<center><h1>登录成功！</h1>");
+        }else{
+            out.print("<center><h1>登录失败！账户或密码错误！</h1><br>" +
+                    "<a href='login.html'>点此返回</a></center>");
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
