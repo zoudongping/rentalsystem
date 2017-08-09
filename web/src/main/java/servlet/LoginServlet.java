81df3918248d8f25c2f7e7120c09d301cfc07c91
9yuan9 <xieshuyuanzh@163.com>
2017-08-09 16:05:32 +0800

    提交登录servlet的手机号登录或用户名登录

--- web/src/main/java/servlet/LoginServlet.java
+++ web/src/main/java/servlet/LoginServlet.java
@@ -25,12 +25,20 @@ public class LoginServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         String account = request.getParameter("username");
         String pwd = request.getParameter("password");
-        UserInfo userInfo=new UserInfo();
-        userInfo.setUname(account);
-        userInfo.setPwd(pwd);
-        UserInfo user=dao.findUserNameAndPwd(userInfo);
-        if(user!=null){
+        UserInfo userInfo1=new UserInfo();
+        userInfo1.setUname(account);
+        userInfo1.setPwd(pwd);
+        UserInfo user=dao.findUserNameAndPwd(userInfo1);
+        UserInfo userInfo2=new UserInfo();
+        userInfo2.setTel(account);
+        userInfo2.setPwd(pwd);
+        UserInfo user1=dao.findUserTelAndPwd(userInfo2);
+        if(user !=null || user1 !=null){
             out.print("<center><h1>登录成功！</h1>");
+            request.setAttribute("username", user);
+            request.getSession().setAttribute("username", user);
+            request.setAttribute("username", user1);
+            request.getSession().setAttribute("username", user1);
         }else{
             out.print("<center><h1>登录失败！账户或密码错误！</h1><br>" +
                     "<a href='login.html'>点此返回</a></center>");
