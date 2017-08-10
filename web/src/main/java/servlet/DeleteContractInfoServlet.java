791629852ef14323558cdad0ca3601567096334b
1234567cao <fujiaxing00>
2017-08-10 15:32:09 +0800

    删除

---
+++ web/src/main/java/servlet/DeleteContractInfoServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import dao.ContractInfoDao;
+import dao.SqlSessionHelper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by THINK on 2017/8/10.
+ */
+@WebServlet(name = "DeleteContractInfoServlet",value = "/deletecontractinfo")
+public class DeleteContractInfoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cid=request.getParameter("cid");
+        ContractInfoDao d= SqlSessionHelper.getSqlSession().getMapper(ContractInfoDao.class);
+        int bo=d.deleteContract(Integer.getInteger(cid));
+        SqlSessionHelper.getSqlSession().commit();
+        SqlSessionHelper.closeSession();
+        if(bo==1){
+            response.sendRedirect("getcontractinfo");
+        }else {
+            response.getWriter().append("sorry!System error!/(ToT)/~~");
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            doPost(request,response);
+    }
+}
