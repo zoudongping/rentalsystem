a313276b26e8b80a62d6006f9742ac5493a735f6
1234567cao <fujiaxing00>
2017-08-14 19:21:29 +0800

    合同模块

--- dao/src/main/java/dao/ContractInfoDao.java
+++ dao/src/main/java/dao/ContractInfoDao.java
@@ -22,6 +22,7 @@ public interface ContractInfoDao {
     public int updateContract(ContractInfo contractinfo);
 
     public int insert(ContractInfo contractInfo);
+
     public ContractInfo findByName(String name);
 
     public List<ContractInfo> findByUser(String sname);
