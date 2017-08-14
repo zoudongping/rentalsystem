c64839b36e13ff5b8c678f0e2214fe91d000d967
1234567cao <fujiaxing00>
2017-08-14 20:21:20 +0800

    合同模块

---
+++ dao/src/main/java/entity/ContractStatusInfo.java
@@ -0,0 +1,40 @@
+package entity;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 付嘉星　　(σ｀д′)σ　　　                                                                 　|
+ * |　@author 都TM闪开，星爷要开始写程序了                                                   |
+ * |　@create 2017年08月14日 - 19:55
+ * |    @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class ContractStatusInfo {
+    private int sid;
+    private String sname;
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    @Override
+    public String toString() {
+        return "ContractStatusInfo{" +
+                "sid=" + sid +
+                ", sname='" + sname + '\'' +
+                '}';
+    }
+}
